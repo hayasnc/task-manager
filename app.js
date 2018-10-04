6d1a2dfef4682f405e0fb45a6ce2d15581706bb2
hayas <hayasnc@gmail.com>
2018-10-04 15:33:43 +0530

    port dynamic heroku issue fix

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/tasks', task);
 
-let port = 3000;
 
-app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+app.listen(process.env.PORT || 5000, function() {
+    console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
